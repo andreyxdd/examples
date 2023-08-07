47522bba4891844f52558fb588ce9b15f636e5f5
unknown <andreyxdd@yandex.ru>
2023-08-06 20:17:59 -0600

    Added new folders and corresponding readme files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# examples
-TON JS examples
+# TON JS Examples
+
+This repo contains some common examples of how the Tonweb SDK can be utilized to interact with the [TON Blockchain](https://docs.ton.org/learn/overviews/ton-blockchain).
+
+Tonweb is a Javascript SDK for The Open Network built upon [Toncenter API](https://toncenter.com/).
+
+List of examples:
+- [Creating a key pair and a wallet](./key-pair-and-wallet/README.md)
+- [Subscribing to account transactions](./account/README.MD)
+- [Subscribing to blocks](./block/README.MD)
---
+++ key-pair-and-wallet/index.js
@@ -0,0 +1,42 @@
+import TonWeb from "tonweb";
+import tonMnemonic from "tonweb-mnemonic";
+
+const createKeyPair = async () => {
+    /** @type {string[]} */
+    const words = await tonMnemonic.generateMnemonic();
+
+    /** @type {Uint8Array} */
+    const seed = await tonMnemonic.mnemonicToSeed(words);
+
+    /** @type {nacl.SignKeyPair} */
+    const keyPair = TonWeb.utils.nacl.sign.keyPair.fromSeed(seed);
+
+    console.log(TonWeb.utils.bytesToHex(keyPair.publicKey));
+    console.log(TonWeb.utils.bytesToHex(keyPair.secretKey));
+
+    /** @type {nacl.SignKeyPair} */
+    const keyPair2 = TonWeb.utils.nacl.sign.keyPair();
+
+    console.log(TonWeb.utils.bytesToHex(keyPair2.publicKey));
+    console.log(TonWeb.utils.bytesToHex(keyPair2.secretKey));
+}
+
+createKeyPair();
+
+
+/**
+ * @param keyPair {nacl.SignKeyPair}
+ */
+const createWallet = async (keyPair) => {
+    const tonweb = new TonWeb();
+    const WalletClass = tonweb.wallet.all.v3R2;
+    const wallet = new WalletClass(tonweb.provider, {
+        publicKey: keyPair.publicKey
+    });
+
+    /** @type {Address} */
+    const address = await wallet.getAddress();
+    console.log(address.toString(true, true, true));
+}
+
+createWallet(TonWeb.utils.nacl.sign.keyPair());
